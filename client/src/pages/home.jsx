f96b4f95dd3fc2dc9583f99525200f346d61fe31
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-09-10 11:02:23 +0200

    fetch img with url

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const  [imgUrl, setImgUrl] = useState();
+    useEffect( ()  => {
+        let id = 1;
+        fetch(`http://localhost:8000/${id}`)
+            .then(response => {
+                if(response.ok){
+                    return response.blob();
+                }
+                throw response;
+            })
+            .then(imageBlob => {
+                const urlImageBlob = URL.createObjectURL(imageBlob);
+                setImgUrl(urlImageBlob);
+            })
+            .catch(err => {
+                console.error("Error :", err);
+            })
+
+    }, []);
+
     return (
         <Fragment>
-
+            <image src={imgUrl} alt="image de coussin"/>
         </Fragment>
     )
 };
